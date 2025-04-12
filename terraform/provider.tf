4ee7be493727302cf95250d9cf326e5c4e215835
Mohammad Arif <33518468+onlyaarif@users.noreply.github.com>
2025-04-13 00:40:30 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,8 +4,9 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# This stays fixed for Lambda@Edge deployment
 provider "aws" {
   alias  = "virginia"
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
