b8ded3c13c62fe9ba135d724909329e4eafb22c7
Mohammad Arif <33518468+onlyaarif@users.noreply.github.com>
2025-04-20 01:48:49 +0530

    Fix: use dynamic IAM role name based on environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
  resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role_arif"
+  name = "lambda_exec_role_${var.environment}"
 
   assume_role_policy = jsonencode({
   Version = "2012-10-17"
